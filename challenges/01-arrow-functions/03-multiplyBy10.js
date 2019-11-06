c665b470257ed102ce5a37b869b320cfd8ccbd30
loganmulvihill <mulvihill.logan@gmail.com>
2019-11-06 12:52:08 -0800

    1

--- challenges/01-arrow-functions/03-multiplyBy10.js
+++ challenges/01-arrow-functions/03-multiplyBy10.js
@@ -1,7 +1,7 @@
 //multiplyBy10 has 1 parameter. Convert it to an arrow function without wrapping the paramater in () as well as utilize the
 //implicit return.
 
-const multiplyBy10 = (num) => num * 10;
+const multiplyBy10 = (num) => num * 10; 
 
 
 
